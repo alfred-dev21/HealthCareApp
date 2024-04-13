64b923bc72ac7ff61d6a6dcc7bc7830c4ba5fe0f
Alfred Khanyile <mr.khanyile21@gmail.com>
2024-04-13 10:56:33 +0000

    added a UI for RegistrationActivity added additional drawables that are applicable to the application itself linked the login and registration activities. The app itself is still nt functional as a single app.

--- app/src/main/java/com/example/healthcareapp/RegistrationActivity.java
+++ app/src/main/java/com/example/healthcareapp/RegistrationActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 
-    public void toRegistration(View view) {
-        Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+    public void toLogin(View view) {
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 }
