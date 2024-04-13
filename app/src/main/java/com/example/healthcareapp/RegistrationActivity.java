0c4a5a4d93f7c7437d5ae3e77339ecca5cd01628
Alfred Khanyile <mr.khanyile21@gmail.com>
2024-04-13 17:17:34 +0000

    made changes to the UI string names

--- app/src/main/java/com/example/healthcareapp/RegistrationActivity.java
+++ app/src/main/java/com/example/healthcareapp/RegistrationActivity.java
@@ -22,6 +22,7 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        database = new DatabaseHelper(this, "app");
     }
 
     public void toLogin(View view) {
